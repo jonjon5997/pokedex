19261a6cfa015efb0c015053e31b6d2a5db2276e
Jonathan Sanfilippo <jonjon5997@gmail.com>
2025-02-26 10:27:51 -0500

    include useEffect for pokedex functionality and useState for state variables and import axios and use signal instead of cancelToken and AbortController in useEffect

---
+++ src/Pagination.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Pagination({ goToNextPage, goToPrevPage }) {
+  return (
+    <div>
+      <button onClick={goToPrevPage}>Previous</button>
+      <button onClick={goToNextPage}>Next</button>
+    </div>
+  );
+}
